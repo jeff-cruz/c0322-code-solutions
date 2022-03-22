7913dbee3b02e24cd97cc39ea51f07a83ad75843
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-22 20:38:35 +0000

    In Progress

---
+++ javascript-functions/main.js
@@ -0,0 +1,7 @@
+function convertMinutesToSeconds(x) {
+  var seconds = x * 60;
+  return seconds;
+}
+
+var convertMinutesToSecondsResult = convertMinutesToSeconds(5);
+console.log('convertMinutesToSecondsResult:', convertMinutesToSecondsResult);
