969fd3c1ca169e88c96a4db52657969e65287c8e
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-25 20:22:25 +0000

    Corrected stringmanipulation-1

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,20 +1,13 @@
 /* exported getWords */
 function getWords(string) {
-  var array = [];
   var word = string.split(' ');
-  for (var i = 0; i < word.length; i++) {
-    if (string === '') {
-      array.push();
-    } else {
-      array.push(word[i]);
-    }
+  if (string !== '') {
+    return word;
   }
-  return array;
+  return [];
 }
 
 // 1. create new Array
 // 2. split string and assign new value to new variable word
-// 3. access each index of string
-// 4. if string has no words, push ' ' into array
-// 5. otherwise, push word into array
-// 6. return array
+// 3. if string is not an empty string return the word
+// 4. if string is empty, return an empty array
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,11 +1,15 @@
 /* exported isVowel */
 function isVowel(character) {
-  if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u' || character === 'A' || character === 'E' || character === 'I' || character === 'O' || character === 'U') {
-    return true;
+  var vowel = ['a', 'e', 'i', 'o', 'u'];
+  for (var i = 0; i < vowel.length; i++) {
+    if (character === vowel[i] || character === vowel[i].toUpperCase()) {
+      return true;
+    }
   }
   return false;
 }
 
-// 1. if character does not equal to any string of 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
-// 2. return true
-// 3. else return false
+// assign an array of vowels to a variable vowels
+// if character is a vowel or uppercase vowel
+// return true
+// otherwise return false
