d6835e4a8f0ca910cb91e3fd0cebbb95f086ec19
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-25 22:07:31 +0000

    Completed array-manipulation-1

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,15 @@
 /* exported compact */
+function compact(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+// create new array
+// access value of every index of the array
+// if that value is a truthy
+// push array value into new array
--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -1,1 +1,14 @@
 /* exported reverse */
+function reverse(array) {
+  var newArray = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+// create new array
+// access the value of the index of the array
+// start with the last index of the array
+// push value into new array
+// return new array
