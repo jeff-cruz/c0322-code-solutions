9864714a08e03d68f82984f8fc43182daae466f6
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-26 00:34:14 +0000

    In progress

---
+++ string-manipulation-2/src/truncate.js
@@ -0,0 +1,13 @@
+/* exported truncate */
+/* function truncate(length, string) {
+  for (var i = 0; i < string.length; i++) {
+    var newString = '';
+
+    if (string[i] < length) {
+      var character = string[i];
+      var ellipsis = '...';
+
+    }
+  }
+  return phrase;
+} */
