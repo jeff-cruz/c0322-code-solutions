5af0fa9536de71577bca901901b7062696034ca9
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-26 00:43:22 +0000

    Corrected object-manipulation-1

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,8 +1,7 @@
 /* exported setValue */
 function setValue(object, key, value) {
   object[key] = value;
-  return object.key;
 }
 
 // set object key to value
-// return key property of object
+// this function does not return anything
