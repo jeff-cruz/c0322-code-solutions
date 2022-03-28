9ee20634fc02f511840a2a7afb04fe031a5507f6
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-28 16:54:20 +0000

    In progress

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,11 @@
 /* exported capitalizeWord */
+function capitalizeWord(word) {
+  var firstLetter = word[0].toUpperCase();
+  var incompleteWord = word.slice(1);
+  var lowercasedWord = incompleteWord.toLowerCase();
+  var newWord = firstLetter + lowercasedWord;
+  if (word !== 'javascript') {
+    return newWord;
+  }
+  return 'Javascript';
+}
--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,13 +1,6 @@
 /* exported truncate */
-/* function truncate(length, string) {
-  for (var i = 0; i < string.length; i++) {
-    var newString = '';
-
-    if (string[i] < length) {
-      var character = string[i];
-      var ellipsis = '...';
-
-    }
-  }
-  return phrase;
-} */
+function truncate(length, string) {
+  var newString = string.slice(0, length);
+  var ellipsis = '...';
+  return newString + ellipsis;
+}
