4358d7a87eddc86472e0cee80a61b0c31d295c33
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-29 16:55:32 +0000

    In progress

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,11 +1,20 @@
 /* exported capitalizeWord */
 function capitalizeWord(word) {
-  var firstLetter = word[0].toUpperCase();
-  var incompleteWord = word.slice(1);
-  var lowercasedWord = incompleteWord.toLowerCase();
-  var newWord = firstLetter + lowercasedWord;
-  if (word !== 'javascript') {
-    return newWord;
+  var lowercase = word.toLowerCase();
+  if (lowercase !== 'javascript') {
+    var firstLetter = lowercase[0].toUpperCase();
+    var lastPart = lowercase.slice(1);
+    var uppercase = firstLetter + lastPart;
+    return uppercase;
   }
-  return 'Javascript';
+  return 'JavaScript';
 }
+
+// create a variable lowerCase to lowercase the whole word
+// if word does not equal javascript then capitalize first letter of word
+// result of that expression assigned to new variable firstLetter
+// slice rest of word after first letter and assign to new variable lastPart
+// concatenate firstLetter with lastPart and assign result to new variable uppercase
+// return uppercase
+// if does equal javascript
+// return JavaScipt
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,16 @@
 /* exported ransomCase */
+function ransomCase(string) {
+  for (var i = 1; i < string.length; i++) {
+    var lowercase = string.toLowerCase();
+    var newString = '';
+    if (lowercase[i % 2 !== 0]) {
+      newString += lowercase;
+    }
+    newString += lowercase.charAt(i).toUpperCase();
+  }
+  return newString;
+}
+
+// create new variable assigned to an empty string
+// lowercase string, result of that assign to new variable lowercase
+//
