62432b9085cee300a4aa61bc3852565b7775f601
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-29 00:06:20 +0000

    Completed dom-manipulation

---
+++ dom-manipulation/main.js
@@ -0,0 +1,23 @@
+var totalClicks = 1;
+var $hotButton = document.querySelector('.hot-button');
+var $clickCount = document.querySelector('.click-count');
+
+$hotButton.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  $clickCount.textContent = 'Clicks: ' + totalClicks++;
+
+  if (totalClicks < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (totalClicks < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (totalClicks < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (totalClicks < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (totalClicks < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
+}
