077d260d9379882747455bc22ff808fff816eaea
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-29 21:40:26 +0000

    Completed javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -3,13 +3,23 @@ var $background = document.querySelector('body');
 
 $button.addEventListener('click', buttonClick);
 
+var isLightOn = false;
+
 function buttonClick(event) {
-  if ($button.className === 'circle light-button off') {
+  if (isLightOn === false) {
     $button.className = 'circle light-button on';
     $background.className = 'on';
+    isLightOn = true;
   } else {
     $button.className = 'circle light-button off';
     $background.className = 'off';
+    isLightOn = false;
   }
-
 }
+
+// create a new variable button for dom selector of class light-button
+// creata a new variable background for dom selector of the body element
+// add eventlistener to new variable button for any clicks
+// when button is clicked:
+// if button is off, turn button on
+// else, turn it off
