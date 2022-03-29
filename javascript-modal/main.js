650e3df568d1457a2acbc2c2ddc76454bc8bea98
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-29 23:45:37 +0000

    Completed javascript-modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,7 +1,22 @@
-var $button = document.querySelector('.modal-button');
+var $openButton = document.querySelector('.modal-open');
+$openButton.addEventListener('click', clickOpen);
 
-$button.addEventListener('click', clickButton);
+var $closeButton = document.querySelector('.modal-close');
+$closeButton.addEventListener('click', clickClose);
 
-function clickButton(event) {
+var $overlay = document.querySelector('.overlay');
 
+var noModal = true;
+function clickOpen(event) {
+  if (noModal !== true) {
+    $overlay.className = 'overlay no-display';
+  }
+  $overlay.className = 'overlay display';
+}
+
+function clickClose(event) {
+  if (noModal !== true) {
+    $overlay.className = 'overlay display';
+  }
+  $overlay.className = 'overlay no-display';
 }
