9de7289a1bd97ecd36cd9e8e9bcd028117c16ace
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-30 04:07:28 +0000

    In progress

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,15 @@
+// var $letters = document.querySelectorAll('span');
+
+// // $letters.addEventListener('keydown', logKey);
+// // var totalSpan = 30;
+
+// function logKey(keydown) {
+//   console.log('typing...');
+// for (var i = 0; i < totalSpan; i++) {
+//   if (input === $letter[i]) {
+//     $letter.className = 'green';
+//   } else if (input !== $letter[i]) {
+//     $letter.className = 'red';
+//   }
+// }
+// }
