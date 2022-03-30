e411d5e5e2fce09157b7c2f7e572a4bc3405a932
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-30 21:28:48 +0000

    In progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,15 +1,23 @@
 // var $letters = document.querySelectorAll('span');
 
-// // $letters.addEventListener('keydown', logKey);
-// // var totalSpan = 30;
+// for (var i = 0; i < $letters.length; i++) {
+//   console.log($letters.forEach());
+// }
+
+// function logKey(event) {
+//   console.log('keydown');
+// }
+
+// var totalSpan = 30;
+// var string = 'grumpy wizards make toxic brew';
 
 // function logKey(keydown) {
-//   console.log('typing...');
-// for (var i = 0; i < totalSpan; i++) {
-//   if (input === $letter[i]) {
-//     $letter.className = 'green';
-//   } else if (input !== $letter[i]) {
-//     $letter.className = 'red';
-//   }
-// }
+// //   console.log('typing...');
+// // for (var i = 0; i < totalSpan; i++) {
+// //   if (input === $letter[i]) {
+// //     $letter.className = 'green';
+// //   } else if (input !== $letter[i]) {
+// //     $letter.className = 'red';
+// //   }
+// // }
 // }
