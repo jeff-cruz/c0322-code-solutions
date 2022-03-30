cffb37aafe85eea2f837a8e431b150543daa1103
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-30 17:21:23 +0000

    In progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,21 @@
 /* exported capitalizeWords */
+function capitalizeWords(string) {
+  var words = string.split(' ');
+  var output = [];
+  for (var word of words) {
+    var firstLetter = word.substring(0, 1).toUpperCase();
+    var otherLetters = word.substring(1).toLowerCase();
+    var wholeWord = firstLetter + otherLetters;
+    output.push(wholeWord);
+  }
+  return output.join(' ');
+}
+
+// create new variable words assigned to string splitting each word by itself into an array
+// create a new variable output as an empty array
+// access each string in the array of strings
+// create a new variable firstLetter and assign it to the first index of the string being capitalized
+// create a new variable otherLetters and assign it to the rest of the words being lowercased
+// create a new variable wholeWord concatenating firstLetter and otherLetters
+// push wholeWord into the output array
+// return the output array back into a string with spaces between the words
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,16 +1,20 @@
 /* exported ransomCase */
 function ransomCase(string) {
-  for (var i = 1; i < string.length; i++) {
-    var lowercase = string.toLowerCase();
-    var newString = '';
-    if (lowercase[i % 2 !== 0]) {
-      newString += lowercase;
+  var input = string.toLowerCase();
+  var output = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 !== 0) {
+      output += input[i].toUpperCase();
+    } else {
+      output += input[i];
     }
-    newString += lowercase.charAt(i).toUpperCase();
   }
-  return newString;
+  return output;
 }
 
-// create new variable assigned to an empty string
-// lowercase string, result of that assign to new variable lowercase
-//
+// create new variable input assigned to the string being lowercased
+// create new variable output assigned to an empty string
+// access every index of the string
+// if remainder of i does not equal zero, capitalize the letter and add it to the output
+// if remainder of i equals zero, add letter to the output
+// return output
