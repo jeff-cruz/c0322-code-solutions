03d4d6725b6511dc1ea937be3e32255d76812588
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-03-31 18:42:49 +0000

    in progress

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,15 @@
 /* exported numVowels */
+function numVowels(string) {
+  var counter = 0;
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  for (var letter of string.toLowerCase()) {
+    if (vowels.includes(letter)) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+// create a new variable counter to 0
+// create an array of vowels and assign to new variable vowels
+// iterate through index of string
