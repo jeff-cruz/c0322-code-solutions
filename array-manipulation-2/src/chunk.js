4a05da68904a3dcb3a11904bd5d132e295e2e960
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-01 21:30:11 +0000

    In Progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,12 @@
 /* exported chunk */
+// function chunk(array, size) {
+//   var newArray = [];
+//   var arrayArray = [];
+//   for (var j = 0; j < array.length; j++) {
+//     for (var i = 0; i < size; i++) {
+//       newArray.push(array[i]);
+//       arrayArray.push(newArray[j]);
+//     }
+//   }
+//   return newArray;
+// }
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,9 @@
 /* exported dropRight */
+function dropRight(array, count) {
+  var newArray = array.slice(array[-1], array.length - count);
+  return newArray;
+}
+
+// assign the result of the following expression to a new variable newArray
+// slice from the first index of array to the result of the length of array minus count
+// return array
