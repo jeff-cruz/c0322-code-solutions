b1768a22f1758a5b44e0f21cdd7e143311019098
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-10 22:27:51 +0000

    In progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,17 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var newString = '';
+  var splitString = string.split(' ');
+  for (var i = 0; i < splitString.length; i++) {
+    var words = splitString[i];
+    var letters = words.split('');
+  }
+  for (var j = letters.length; j > letters.length; j--) {
+  }
+  console.log(words);
+}
+
+// split the string by sections of words
+// for every word, split word by letter
+// for every index, letters are split up
+// for every index,
