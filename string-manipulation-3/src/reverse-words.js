322f0512b12fd7293664a72e8555bf8290d13f86
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-12 02:34:29 +0000

    In progress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -6,9 +6,7 @@ function reverseWords(string) {
   for (var i = 0; i < splitString.length; i++) {
     if (i === 0) {
       newWord = splitString[i];
-    } else if (i > 0 && i < splitString.length - 1) {
-      newWord = splitString[i] + ' ';
-    } else if (i === splitString.length - 1) {
+    } else if (i > 0 && i <= splitString.length - 1) {
       newWord = splitString[i] + ' ';
     }
     var letters = newWord.split('');
@@ -19,7 +17,13 @@ function reverseWords(string) {
   return newPhrase;
 }
 
-// split the string by sections of words
-// for every word, split word by letter
-// for every index, letters are split up
-// for every index, letters are reversed
+// create a new variable newWord assigned to an empty string
+// create a variable splitString assigned to the result of the split method of the string at every space
+// iterate through splitString
+// at the first index, add the word to the newWord string
+// every index after, add the word plus a space
+// assign new variable letters to every letter of newWord split
+// iterate through letters from end of string to beginning
+// add each letter from last index to first
+//  assign result to new variable newPhrase
+// return newPhrase
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,30 @@
 /* exported titleCase */
+function titleCase(string) {
+  var words = string.toLowerCase().split(' ');
+  var newTitle = '';
+  var fullWord = '';
+
+  for (var i = 0; i < words.length; i++) {
+    var firstIndexCapital = words[i][0].toUpperCase();
+    var restOfWord = words[i].substring(1);
+
+    if (i !== words.length - 1) {
+      fullWord = firstIndexCapital + restOfWord + ' ';
+      if (fullWord.length <= 2) {
+        fullWord.toLowerCase();
+      }
+      newTitle += fullWord;
+    } else {
+      fullWord = firstIndexCapital + restOfWord;
+      newTitle += fullWord;
+    }
+  }
+  console.log(newTitle);
+  return newTitle;
+}
+
+// Capitalize the first word of the title and of any subtitle.
+// Capitalize all “major” words(nouns, verbs, adjectives, adverbs, and pronouns) in the title, including the second part of hyphenated major words(e.g., Self - Report not Self - report).
+// Capitalize all words of four letters or more.
+// JavaScript is always cased JavaScript.
+// API is a "known" acronym.
