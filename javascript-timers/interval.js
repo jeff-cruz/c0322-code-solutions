709688f5d10340c4ce135c8318c0a1db1882b23f
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-13 17:55:44 +0000

    Corrected javascript-timers

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -2,13 +2,9 @@ var $h1 = document.querySelector('.countdown-display');
 setInterval(decrement, 1000);
 
 function decrement() {
-  if ($h1.textContent === '4') {
-    $h1.textContent = '3';
-  } else if ($h1.textContent === '3') {
-    $h1.textContent = '2';
-  } else if ($h1.textContent === '2') {
-    $h1.textContent = '1';
-  } else if ($h1.textContent === '1') {
+  if ($h1.textContent > 1) {
+    $h1.textContent--;
+  } else {
     $h1.textContent = '~Earth Beeeelooowww Us~';
   }
 }
