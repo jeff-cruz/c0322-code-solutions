e3db7b68222d2666efebfd97c6ce625e3f402d50
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-14 03:34:04 +0000

    Set interval

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,16 +1,10 @@
-setInterval(nextPokemon, 3000);
+var intervalID = setInterval(nextPokemon, 3000);
 var pokemon = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
-
 var $rightArrow = document.querySelector('#right-arrow');
 var $leftArrow = document.querySelector('#left-arrow');
 var $img = document.querySelector('#image');
 var $dotContainer = document.querySelector('.dot-container');
-var $button = document.querySelector('.button');
-var $firstDot = document.querySelector('#firstDot');
-var $secondDot = document.querySelector('#secondDot');
-var $thirdDot = document.querySelector('#thirdDot');
-var $fourthDot = document.querySelector('#fourthDot');
-var $fifthDot = document.querySelector('#fifthDot');
+var $buttonList = document.querySelectorAll('.button');
 
 $rightArrow.addEventListener('click', nextPokemon);
 function nextPokemon(event) {
@@ -40,36 +34,21 @@ function previousPokemon(event) {
 
 $dotContainer.addEventListener('click', dotClick);
 function dotClick(event) {
-  if (!event.target.tagName === 'I') {
-    return false;
-  } else {
-    if (event.target.id === 'firstDot') {
-      $img.setAttribute('src', pokemon[0]);
-      $button.className = 'far fa-circle button';
-      $firstDot.className = 'fas fa-circle button';
-    } else if (event.target.id === 'secondDot') {
-      $img.setAttribute('src', pokemon[1]);
-      $button.className = 'far fa-circle button';
-      $secondDot.className = 'fas fa-circle button';
-    // } else if (event.target.id === 'thirdDot') {
-    //   $img.setAttribute('src', pokemon[2]);
-    //   $thirdDot.setAttribute('class', 'fas fa-circle button');
-    //   $button.setAttribute('class', 'far fa-circle button');
-    // } else if (event.target.id === 'fourthDot') {
-    //   $img.setAttribute('src', pokemon[3]);
-    //   $fourthDot.setAttribute('class', 'fas fa-circle button');
-    //   $button.setAttribute('class', 'far fa-circle button');
-    // } else if (event.target.id === 'fifthDot') {
-    //   $img.setAttribute('src', pokemon[4]);
-    //   $fifthDot.setAttribute('class', 'fas fa-circle button');
-    //   $button.setAttribute('class', 'far fa-circle button');
+  if (event.target.tagName === 'I') {
+    for (var i = 0; i < $buttonList.length; i++) {
+      $buttonList[i].className = 'far fa-circle button';
+      if (i === parseInt(event.target.getAttribute('id'))) {
+        $buttonList[i].className = 'fas fa-circle button';
+        $img.setAttribute('src', pokemon[i]);
+      }
     }
   }
 }
 
-// TUTOR QUESTIONS:
-// Every time I click the dot button to view the corresponding image, the dots stay
-// black even when I change the className to be the empty circle?
+// current image # var current = 0
+// create for loop, iterate over nodelist, reset class name to make it empty, if current === i, class name to fill in circle
 
+// TUTOR QUESTIONS:
 // Setting interval time and showing the next image works, but
-// if I click to another image, it does not reset the 3 seconds
+// if I click to another image, it does not reset the 3 seconds?
+// something about clearInterval(intervalID)
