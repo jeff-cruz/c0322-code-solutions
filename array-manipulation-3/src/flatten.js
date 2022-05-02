d5130ecf2033ba75dc97862e43018ba1f1fab389
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-05-02 05:42:16 +0000

    In Progress

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -5,11 +5,19 @@ function flatten(array) {
     var currentIndex = array[i];
     if (!(Array.isArray(currentIndex))) {
       newArray.push(currentIndex);
+    } else {
+      for (var j = 0; j < currentIndex.length; j++) {
+        newArray.push(currentIndex[j]);
+      }
     }
   }
+  return newArray;
 }
 
 // create empty array assign to new variable
 // loop through array
 // create a new variable currentIndex assigned to array at i
-// if array is not an array, push index to new array
+// if array is not an array, push index to newArray
+// else, loop through array in currentIndex
+// push indexes of array into newArray
+// return newArray
