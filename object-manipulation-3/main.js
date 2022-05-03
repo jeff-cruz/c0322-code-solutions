e332bb8d51845212288d53c10faedfbd603ecab1
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-05-03 06:25:53 +0000

    Completed object-manipulation-3

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,22 +1,20 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// players
 var players = [
   {
-    name: 'Donatello',
-    hand: null
+    name: 'Donatello'
   },
   {
-    name: 'Leonardo',
-    hand: null
+    name: 'Leonardo'
   },
   {
-    name: 'Raphael',
-    hand: null
+    name: 'Raphael'
   },
   {
-    name: 'MichaelAngelo',
-    hand: null
+    name: 'MichaelAngelo'
   }];
 
+// deck of cards
 var cards = [
   {
     rank: 'Ace',
@@ -280,6 +278,7 @@ var cards = [
   }
 ];
 
+// shuffle cards and deal cards
 function shuffleThenDeal(cards) {
   var hand = [];
   var twoCards = 2;
@@ -291,22 +290,75 @@ function shuffleThenDeal(cards) {
     } else if (hand.length === 1) {
       hand.push(shuffledDeck[i]);
     }
-    // console.log(hand);
   }
   return hand;
 }
 
-function giveCards() {
-  for (var i = 0; i < players.length; i++) {
-    var cardsInHand = shuffleThenDeal(cards);
-    players[i].hand = cardsInHand;
-    console.log(players[i].hand);
+// play game
+function playGame(players) {
+  // give players cards
+  for (var player of players) {
+    var hand = shuffleThenDeal(cards);
+    player.hand = hand;
+
+    // determine the worth of the cards for each player
+    var worth = 0;
+    for (var card of player.hand) {
+      worth = worth + card.worth;
+    }
+    player.worth = worth;
   }
+
+  // create array of players cards worth totals
+  var playersWorth = [];
+  for (var i = 0; i < players.length; i++) {
+    playersWorth.push(players[i].worth);
+  }
+
+  // whoever has highest total wins
+  var highestWorth = playersWorth[0];
+  var highestWorthIndex = 0;
+  for (var j = 1; j < playersWorth.length; j++) {
+    if (playersWorth[j] > highestWorth) {
+      highestWorth = playersWorth[j];
+      highestWorthIndex = j;
+    }
+  }
+
+  var winner = players[highestWorthIndex];
+  console.log(winner.name, 'is the winner!');
 }
 
-// Create a collection of four objects that represent players. Each player has a unique name and a hand to hold cards in.
-// Create a collection of fifty - two objects that represent a standard 52 - card deck.Each card should have a rank(Ace, 2, 3, ...King) and a suit(clubs, diamonds, hearts, spades).
-// Shuffle the deck.
-// Deal two cards to each player.No two players should be able to receive the same card.
-// Find the player with the highest score.Each card is worth a specific point value.Aces are worth 11 points, face ranks(Jack, Queen, King) are worth 10 points, and number ranks are worth their value(2, 3, ... 10).
-// Log the winning player to the console.
+playGame(players);
+
+// 1.Create a collection of four objects that represent players. Each player has a unique name and a hand to hold cards in
+//    - create an array named players, each index being an object with a name property and a name value
+// 2. Create a collection of fifty - two objects that represent a standard 52 - card deck.Each card should have a rank(Ace, 2, 3, ...King) and a suit(clubs, diamonds, hearts, spades).
+//    - create an array named cards, each index being an object with:
+//        - a rank property with a string value of the name of the card
+//        - a suit property with a string value of the suit of card
+//        - a worth property with a number value of how much the card is worth
+// 3. Shuffle the deck.
+//    - create a new function definition named shuffleThenDeal with the parameter of cards
+//    - create an empty array and assign it to a new variable named hand
+//    - assign the value 2 to a new variable named twoCards representing the cards in each players hands
+//    - use the shuffle lodash method on the cards array and assign it to a new variable named shuffledCards
+//    - distribute a card by pushing an index of the shuffledCards array to the hand array
+//    - if a player does not have a card in their hand, give them a card
+//    - if a player has one card in their hand, give them another card
+// 4. Deal two cards to each player.No two players should be able to receive the same card.
+//    - create a new function definition named playGame with the parameter of players
+//    - for each player, give them a set of cards by assigning a hand to their hand
+//    - determine the worth of cards for each player
+// 5. Find the player with the highest score.Each card is worth a specific point value.Aces are worth 11 points, face ranks(Jack, Queen, King) are worth 10 points, and number ranks are worth their value(2, 3, ... 10).
+//    - create an empty array assigned to the new variable playersWorth
+//    - for every players hand worth, push the value to the playersWorth array
+//    - assign the highestWorth to the first player's worth
+//    - assign the highestWorthIndex to 0
+//    - loop through playersWorth array
+//    - if playersWorth index value is higher than the first,
+//    - assign highestWorth to the playersWorth index
+//    - assign highestWorthIndex to the same index
+//    - the player at the highestWorthIndex is assigned to the new variable winner
+// 6. Log the winning player to the console.
+//    - log the winner's name to the console
