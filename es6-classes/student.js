2924f620408daf4a24f1dbeda1a8cadb1d3eadfd
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-05-16 17:56:07 +0000

    Completed es6-classes

---
+++ es6-classes/student.js
@@ -0,0 +1,34 @@
+/* exported Student */
+class Student {
+  constructor(firstName, lastName, subject) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.subject = subject;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getIntroduction() {
+    const fullName = this.getFullName();
+    return `Hello, my name is ${fullName} and I am studying ${this.subject}.`;
+  }
+}
+
+// function Student(firstName, lastName, subject) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.subject = subject;
+// }
+
+// Student.prototype.getFullName = function () {
+//   const { firstName, lastName } = this;
+//   return `${firstName} ${lastName}`;
+// };
+
+// Student.prototype.getIntroduction = function () {
+//   const { subject } = this;
+//   const fullName = this.getFullName();
+//   return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+// };
