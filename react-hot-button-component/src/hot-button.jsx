06ceda676a9f82a3b0441486756eddcb75540d0a
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-05-18 22:44:00 +0000

    Completed react-hot-button-component

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -5,24 +5,45 @@ let clicks = 0;
 class HotButton extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = { clickNumber: clicks };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.setState(prevState => ({
-      clickNumber: clicks++
-    }));
+    this.setState({ clickNumber: clicks++ });
   }
 
   render() {
-    return (
-      <button onClick = {this.handleClick}>
-        {this.state.clickNumber}
-      </button>
-    );
+    if (this.state.clickNumber < 3) {
+      return (
+      <button onClick = {this.handleClick}>Hot Button</button>
+      );
+    } else if (this.state.clickNumber < 6) {
+      return (
+        <button className="purple white-text" onClick={this.handleClick}>Hot Button</button>
+      );
+    } else if (this.state.clickNumber < 9) {
+      return (
+        <button className="light-purple white-text" onClick={this.handleClick}>Hot Button</button>
+      );
+    } else if (this.state.clickNumber < 12) {
+      return (
+        <button className="salmon white-text" onClick={this.handleClick}>Hot Button</button>
+      );
+    } else if (this.state.clickNumber < 15) {
+      return (
+        <button className="orange black-text" onClick={this.handleClick}>Hot Button</button>
+      );
+    } else if (this.state.clickNumber < 18) {
+      return (
+        <button className="yellow black-text" onClick={this.handleClick}>Hot Button</button>
+      );
+    } else if (this.state.clickNumber >= 18) {
+      return (
+        <button className="white black-text" onClick={this.handleClick}>Hot Button</button>
+      );
+    }
   }
 }
 
--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import HotButton from './hot-button';
 
-const element = (
-  <div>
-    <HotButton text="Hot Button" />
-  </div>
-);
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-root.render(element);
+root.render(<HotButton />);
