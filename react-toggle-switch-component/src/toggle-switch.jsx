4b634283a986c479d540d03cba397c702d200c63
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-05-19 05:23:56 +0000

    Completed react-toggle-switch-component

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -3,20 +3,30 @@ import React from 'react';
 class ToggleSwitch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isClicked: false };
+    this.state = {
+      toggle: 'slider-background on',
+      text: 'On'
+    };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.setState(prevState => ({
-      isClicked: !prevState.isClicked
-    }));
+    if (this.state.toggle === 'slider-background on') {
+      this.setState({ toggle: 'slider-background off', text: 'Off' });
+    } else {
+      this.setState({ toggle: 'slider-background on', text: 'On' });
+    }
   }
 
   render() {
     return (
-
+      <div className = "row">
+        <div onClick={ this.handleClick } className={this.state.toggle }>
+          <div className = "slider"></div>
+        </div>
+        <p className = "text">{ this.state.text }</p>
+      </div>
     );
   }
 }
