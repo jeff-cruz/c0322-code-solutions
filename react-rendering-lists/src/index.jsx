bfd68ff8dfd038166d5e8e4bc6a7b81dd6c3cc47
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-05-20 17:51:22 +0000

    Completed react-rendering-lists

---
+++ react-rendering-lists/src/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
+function PokemonList(props) {
+  const pokedex = props.pokedex;
+  const listItems = pokedex.map(pokemon =>
+    <ListItem key={pokemon.number.toString()} value={pokemon.name} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<PokemonList pokedex={pokedex} />);
