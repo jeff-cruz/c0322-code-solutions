97e75421134f3b1d2591355182e43f4a96c99ada
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-05-21 00:40:28 +0000

    Completed react-accordion component'

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,24 +1,60 @@
 import React from 'react';
 
+const topics = [
+  {
+    id: '1',
+    name: 'HTML',
+    details: 'Hypertext Markup Language is the standard markuplanguage for creating web pages and web applications.'
+  },
+  {
+    id: '2',
+    name: 'CSS',
+    details: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document.'
+  },
+  {
+    id: '3',
+    name: 'JavaScript',
+    details: 'JavaScript is a high-level interpreted programming language that conforms to the ECMAScript Specification.'
+  }
+];
+
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: false
+      openTopic: null
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    if(this.state.visible === false) {
-      this.setState({ visible: true })
-    } else {
-      this.setState({ visible: false })
+  // when user clicks a topic, update the state to the specific topic index
+  handleClick(event) {
+    if (this.state.openTopic === null) {
+      this.setState({ openTopic: event.target.id });
+    } else if (this.state.openTopic === event.target.id) {
+      this.setState({ openTopic: null });
     }
   }
 
-  render() {
+  render(event) {
     return (
-
+      <div className='container'>
+        <ul>
+          {
+            topics.map((topic, index) => {
+              return (
+                <li key={topic.id}>
+                  <h2 id={index} onClick={this.handleClick} className='topic'>{topic.name}</h2>
+                  <p className={index.toString() === this.state.openTopic
+                    ? 'details'
+                    : 'details hidden'
+                    }> {topic.details}</p>
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
     );
   }
 }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Accordion from './accordion';
 
-const container = document.querySelector('root');
+const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 root.render(<Accordion />);
