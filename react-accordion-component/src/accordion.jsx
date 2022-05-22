b40a6f8f133d7fb31581383b8385e3d086f2442a
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-05-22 02:58:38 +0000

    Corrected react-accordion-component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,23 +1,5 @@
 import React from 'react';
 
-const topics = [
-  {
-    id: '1',
-    name: 'HTML',
-    details: 'Hypertext Markup Language is the standard markuplanguage for creating web pages and web applications.'
-  },
-  {
-    id: '2',
-    name: 'CSS',
-    details: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document.'
-  },
-  {
-    id: '3',
-    name: 'JavaScript',
-    details: 'JavaScript is a high-level interpreted programming language that conforms to the ECMAScript Specification.'
-  }
-];
-
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
@@ -27,32 +9,32 @@ class Accordion extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  // when user clicks a topic, update the state to the specific topic index
   handleClick(event) {
-    if (this.state.openTopic === null) {
-      this.setState({ openTopic: event.target.id });
-    } else if (this.state.openTopic === event.target.id) {
+    const topicID = event.target.id;
+    if (this.state.openTopic === topicID) {
       this.setState({ openTopic: null });
+    } else {
+      this.setState({ openTopic: topicID });
     }
   }
 
   render(event) {
+    const topics = this.props.topics;
+    const data = topics.map(topic =>
+        <li key={topic.id}>
+          <h2 id={topic.id} onClick={this.handleClick} className='topic'>
+            {topic.name}
+          </h2>
+          <p className={this.state.openTopic === topic.id ? 'details' : 'details hidden'}>
+            {topic.details}
+          </p>
+        </li>
+    );
+
     return (
       <div className='container'>
         <ul>
-          {
-            topics.map((topic, index) => {
-              return (
-                <li key={topic.id}>
-                  <h2 id={index} onClick={this.handleClick} className='topic'>{topic.name}</h2>
-                  <p className={index.toString() === this.state.openTopic
-                    ? 'details'
-                    : 'details hidden'
-                    }> {topic.details}</p>
-                </li>
-              );
-            })
-          }
+          {data}
         </ul>
       </div>
     );
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Accordion from './accordion';
 
+const topics = [
+  {
+    id: '1',
+    name: 'HTML',
+    details: 'Hypertext Markup Language is the standard markuplanguage for creating web pages and web applications.'
+  },
+  {
+    id: '2',
+    name: 'CSS',
+    details: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document.'
+  },
+  {
+    id: '3',
+    name: 'JavaScript',
+    details: 'JavaScript is a high-level interpreted programming language that conforms to the ECMAScript Specification.'
+  }
+];
+
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-root.render(<Accordion />);
+root.render(<Accordion topics={topics}/>);
