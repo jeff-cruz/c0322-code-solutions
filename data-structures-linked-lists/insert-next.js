6c109a77c097b18b61450d399c772fd5474f825f
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-06-17 05:09:10 +0000

    Corrected kinda cheating to kinda works

--- data-structures-linked-lists/insert-next.js
+++ data-structures-linked-lists/insert-next.js
@@ -2,11 +2,7 @@
 /* exported insertNext */
 
 function insertNext(list, value) {
-  const third = list.next.data;
-  const fourth = list.next.next.data;
-  const fifth = list.next.next.next.data;
-  list.next = new LinkedList(value);
-  list.next.next = new LinkedList(third);
-  list.next.next.next = new LinkedList(fourth);
-  list.next.next.next.next = new LinkedList(fifth);
+  const newList = new LinkedList(value);
+  newList.next = list.next;
+  list.next = newList;
 }
